255692f91b41f83758537083ff343b3e79e964c0
ChimpChonk <sanjit.poudel28@gmail.com>
2023-04-21 20:19:12 +0200

    Userprofile WIP

---
+++ FakeTinder/Models/UserProfileEntity.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FakeTinder.Models
+{
+	public class UserProfileEntity
+	{
+		public DateTime BirthDate { get; set; }
+		public int Height { get; set; }
+		public string AboutMe { get; set; }
+
+	}
+}
